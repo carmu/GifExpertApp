18bdd2209ec2ec4132370738b853669bbb595265
carlos <camunoz.it@gmail.com>
2021-05-18 16:15:44 -0300

    docs agregados

---
+++ docs/precache-manifest.e624b09bbe7ba8d10d25268029a4e1c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5501aa07c070e430a02bb7ef7380ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12fe3fcd11dc9c785b8e",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fe3fcd11dc9c785b8e",
+    "url": "/static/js/main.5636e1c8.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
